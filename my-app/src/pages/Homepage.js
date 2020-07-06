232c620f2bd8f7b781660984478bbd99c6385fd4
W. Alan Patterson II <walanpat@live.unc.edu>
2020-07-06 19:48:44 -0400

    Push1
    Initializing things, nothing big

---
+++ my-app/src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+
+
+const Homepage = () =>{
+    return (
+        <div>Homepage Stuff</div>
+    );
+}
+
+export default Homepage;

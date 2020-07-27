4bff3992d2d8e16701148d8ff4da41c3c0ef1420
ach1lles11 <42646588+ach1lles11@users.noreply.github.com>
2020-07-27 10:05:11 -0400

    Starting on About
    7-27-20

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -5,17 +5,40 @@ import test1 from './images/CCG.jpg'
 const About = () => {
     return (
         <div>
-            <div className='aboutTest'>
-                <h2> BeInformedNC is a project designed to get voters more informed about the candidates 
-                which they are voting for. We are a group of university students with a focus on nonpartisanship
-                and our sources are very credible.
+            <div className='aboutText'>
+                <h1 style = {{ marginLeft: 150, marginRight: 150 }}>
+                    Mission Statement:
+                </h1>
+                <h2 style = {{ marginLeft: 150, marginRight: 150 }}>
+                    To help North Carolina residents of all ages be more informed of their electoral 
+                    candidates at the state, district, and local level.
+                </h2>
+                <h1 style = {{ marginLeft: 150, marginRight: 150 }}>
+                    Bio:
+                </h1>
+                <h2 style = {{ marginLeft: 150, marginRight: 150 }}> 
+                    Developed by University students; emphasize nonpartisan aspect and where 
+                    we get our sources from 
+                </h2>
+                <h1 style = {{ marginLeft: 150, marginRight: 150 }}>
+                    Goals:
+                </h1>
+                <h2 style = {{ marginLeft: 150, marginRight: 150 }}>
+                    -To help NC residents be more informed about their candidates
+                    <br></br>
+                    -Help NC residents register to vote
+                    <br></br>
+                    -Increase turn out rate for midterm and primary elections
                 </h2>
             </div>
             <div className='aboutPics'>
-            <img src={test} alt="test" />
-            <img src={test1} alt="test1" />
+                <img src={test} alt="test" style = {{ width: 200, height: 200 }} />
+                <img src={test1} alt="test1" style = {{ width: 200, height: 200 }} />
             </div>
-            <h1>Me</h1>
+            <div className="captions">
+                <caption>Me</caption>
+                <caption>Me in Garden</caption>
+            </div>
         </div>
     );
 }

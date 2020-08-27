5652e06f6b0f72dfda9b81a121f4c225b958a9ba
ach1lles11 <42646588+ach1lles11@users.noreply.github.com>
2020-08-27 19:47:55 -0400

    Homepage Update 1
    This is to show Alan

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -9,46 +9,30 @@ var Background = {
     backgroundSize: "cover"
 }
 const Homepage = () =>{
-    return (
-        <div >
-            <div style={Background}>
-            <h3 style={{color: "#0015bc", textAlign: "center"}}>
-                <br></br>
-                <br></br>    
-                Welcome to <span style={{color: "#ff3333"}}> BeInformedNC! </span>
-                Our goal is to help you learn about the candidates that will be
-                <br></br>
-                on your ballot in November. Enter your address to start reading!
-                <br></br>
-                </h3>
-                <br></br>
-                <br></br>
-
-                <h4 style={{color: "#0015bc", textAlign: "center"}}>
-                    Find the candidates on your ballot...
-                    <br></br>
-                    <br></br>
-                <Form>
-                <Form.Group className="formAddress">
-                    <Form.Control type="input" placeholder="Your Address Here" style={{width: 400}}/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                 </Form>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                </h4>
-                <br></br>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className='homePage' style={Background}>
+        <h3 style={{color: "#0015bc", textAlign: "center"}}> 
+          Welcome to <span style={{color: "#ff3333"}}> BeInformedNC! </span>
+        </h3>
+        <h3 style={{textAlign: 'center'}}>
+          Our goal is to help you learn about the candidates that will be
+          on your ballot in November. Enter your address to start reading!
+        </h3>
+        <h4 style={{color: "#0015bc", textAlign: "center"}}>
+          Find the candidates on your ballot...
+            <Form>
+              <Form.Group className="formAddress">
+                <Form.Control type="input" placeholder="Your Address Here" style={{width: 400}}/>
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+        </h4>         
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
